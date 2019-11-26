f4f8619565f76a04e7f46023997339e3711430cb
Nick <nikolayvaklinov@yahoo.com>
2019-11-26 22:32:54 +0000

    variables commit

--- current-java/HelloWorld2/src/Hello.java
+++ current-java/HelloWorld2/src/Hello.java
@@ -1,6 +1,17 @@
 public class Hello {
 
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Nick!");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        System.out.println(myFirstNumber);
+
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int total = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(total);
+        int myLastOne = 1000 - total;
+        System.out.println(myLastOne);
+
     }
 }
