040c895be21895a348e8e262f61bc63059c65856
Nick <nikolayvaklinov@yahoo.com>
2019-11-27 15:09:25 +0000

    pounds to kilograms converter commit

---
+++ current-java/PoundToKilograms/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        double pounds = 200d;
+        double kiloConverter = pounds * 0.45359237d;
+        System.out.println(kiloConverter);
+
+    }
+}
