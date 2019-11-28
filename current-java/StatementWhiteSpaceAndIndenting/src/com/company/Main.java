2a9764e32530ec4793e1afbac08d3c087749b6f5
Nick <nikolayvaklinov@yahoo.com>
2019-11-28 12:42:10 +0000

    Statements, whitespaces and indentation how to organize the code

---
+++ current-java/StatementWhiteSpaceAndIndenting/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int myVariable = 50; // the entire line is a statement, the "myVariable = 50" part is expression
+        myVariable++; // statement
+        myVariable--; // statement
+
+        System.out.println("This is " +
+                "another " +
+                "still more.");
+    }
+}
