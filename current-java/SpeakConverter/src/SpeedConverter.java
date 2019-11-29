039df8f59e397025ed3d68f63ab0263619cb1999
Nick <nikolayvaklinov@yahoo.com>
2019-11-29 11:29:22 +0000

    a little bit polishing

--- current-java/SpeakConverter/src/SpeedConverter.java
+++ current-java/SpeakConverter/src/SpeedConverter.java
@@ -6,6 +6,16 @@ public class SpeedConverter {
             return -1;
         }
         return Math.round(kilometersPerHour / 1.609);
+    }
 
+    public static void printConversion(double kilometersPerHour){
+        if(kilometersPerHour < 0){
+            System.out.println("Invalid Value");
+        }else {
+            long milesPerHour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour +
+                    " km/h = " + milesPerHour +
+                    " mi/h");
+        }
     }
 }
