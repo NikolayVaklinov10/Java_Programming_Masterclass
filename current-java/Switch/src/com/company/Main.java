c481040fcc43a838dc3a0bd8a58c3020b87205c5
Nick <nikolayvaklinov@yahoo.com>
2019-12-01 15:10:31 +0000

    switch statement

---
+++ current-java/Switch/src/com/company/Main.java
@@ -0,0 +1,49 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+//        int switchValue = 3;
+//
+//        switch (switchValue){
+//            case 1:
+//                System.out.println("Value was 1");
+//                break;
+//            case 2:
+//                System.out.println("Value was 2");
+//                break;
+//            default:
+//                System.out.println("Was not 1 or 2");
+//                break;
+//        }
+
+        // Challenge
+        char myChar = 'B';
+
+        switch(myChar){
+
+            case 'A':
+                System.out.println("A was found");
+                break;
+            case 'B':
+                System.out.println("B was found");
+                break;
+            case 'C':
+                System.out.println("C was found");
+                break;
+            case 'D':
+                System.out.println("D was found");
+                break;
+            case 'E':
+                System.out.println("E was found");
+                break;
+            default:
+                System.out.println("Neither was found");
+                break;
+
+
+        }
+
+    }
+}
