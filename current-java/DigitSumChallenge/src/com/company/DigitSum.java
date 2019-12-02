b29551aff38b819fdf09059f9e68b5286c11c9f4
Nick <nikolayvaklinov@yahoo.com>
2019-12-02 17:06:02 +0000

    digit sum challenge

---
+++ current-java/DigitSumChallenge/src/com/company/DigitSum.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class DigitSum {
+//    public static int sumDigits(int number){
+//        if(number>= 10 || number < 0){
+//
+//        }
+//        return -1;
+//    }
+}
