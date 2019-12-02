964cb5a68f63cac187201ed4a16c8173d0c6c23e
Nick <nikolayvaklinov@yahoo.com>
2019-12-02 21:02:47 +0000

    perfect number commit

---
+++ current-java/PerfectNumber/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public static boolean isPerfectNumber(int number){
+        if (number < 1)
+            return false;
+
+        int sum = 0;
+        for(int i = 1; i < number; i++){
+            if(number % i == 0){
+                sum += i;
+            }
+        }
+
+        if(number == sum){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
