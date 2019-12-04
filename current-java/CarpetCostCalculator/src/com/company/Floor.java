beb65af40e1a16781c7fd198dd31d22c1dd82a1a
Nick <nikolayvaklinov@yahoo.com>
2019-12-04 20:12:17 +0000

    Carpet cost challenge

---
+++ current-java/CarpetCostCalculator/src/com/company/Floor.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Floor {
+
+    private double width;
+    private double length;
+
+    // the constructor
+    public Floor(double width, double length) {
+        this.width = width;
+        this.length = length;
+        if(this.width < 0){
+            this.width = 0;
+        }
+
+        if(this.length < 0){
+            this.length = 0;
+        }
+    }
+
+    public double getArea(){
+        return this.width * this.length;
+    }
+}
