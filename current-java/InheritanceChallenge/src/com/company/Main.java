e5b772880c436395adc9db30a0efe4c3fa0f8a7e
Nick <nikolayvaklinov@yahoo.com>
2019-12-05 20:32:38 +0000

    inheritance challenge

---
+++ current-java/InheritanceChallenge/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main extends Object{
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Tesla tesla = new Tesla(36);
+        tesla.steer(45);
+        tesla.accelerate(30);
+        tesla.accelerate(20);
+        tesla.accelerate(-42);
+
+    }
+}
