65bd01daa7d8bf6cf265a28a8051d312333cf91c
Nick <nikolayvaklinov@yahoo.com>
2019-12-06 23:18:41 +0000

    polymorphism challenge commit

---
+++ current-java/Polymorphism_Challenge/src/com/company/Ford.java
@@ -0,0 +1,26 @@
+package com.company;
+
+
+class Ford extends Car{
+    public Ford(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Mitsubishi -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Mitsubishi -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return "Mitsubishi -> brake()";
+    }
+}
+
+
+
