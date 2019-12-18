0fabf89f9b194e0d0ca6c256762d2aac3646a0a6
Nick <nikolayvaklinov@yahoo.com>
2019-12-18 16:16:19 +0200

    Scope commit part 2

--- current-java/Scope/src/com/company/Main.java
+++ current-java/Scope/src/com/company/Main.java
@@ -4,16 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String privateVar = "this is private to main()";
+        String varFour = "this is private to main()";
 
         ScopeCheck scopeInstance = new ScopeCheck();
-        System.out.println("scopeInstance privateVar is " + scopeInstance.getPrivateVar());
-        System.out.println(privateVar);
+        scopeInstance.useInner();
 
-        scopeInstance.timesTwo();
-        System.out.println("***********************************");
         ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
-        innerClass.timesTwo();
+        System.out.println("varThree is not accessible here " + innerClass.varThree);
+
+
+
+//        String privateVar = "this is private to main()";
+//
+//        ScopeCheck scopeInstance = new ScopeCheck();
+//        System.out.println("scopeInstance privateVar is " + scopeInstance.getPrivateVar());
+//        System.out.println(privateVar);
+//
+//        scopeInstance.timesTwo();
+//        System.out.println("***********************************");
+//        ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
+//        innerClass.timesTwo();
 
     }
 }
