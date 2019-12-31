92ef9cda57c442783612f9da6c343def75d8a437
Nick <nikolayvaklinov@yahoo.com>
2019-12-31 19:22:21 +0200

    JavaFX challenge part 4

--- current-java/MyContacts/src/sample/ContactController.java
+++ current-java/MyContacts/src/sample/ContactController.java
@@ -4,7 +4,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import sample.datamodel.Contact;
 
+
 public class ContactController {
+
     @FXML
     private TextField firstNameField;
 
@@ -26,4 +28,21 @@ public class ContactController {
         Contact newContact = new Contact(firstName, lastName, phoneNumber, notes);
         return newContact;
     }
+
+    public void editContact(Contact contact) {
+        firstNameField.setText(contact.getFirstName());
+        lastNameField.setText(contact.getLastName());
+        phoneNumberField.setText(contact.getPhoneNumber());
+        notesField.setText(contact.getNotes());
+    }
+
+    public void updateContact(Contact contact) {
+        contact.setFirstName(firstNameField.getText());
+        contact.setLastName(lastNameField.getText());
+        contact.setPhoneNumber(phoneNumberField.getText());
+        contact.setNotes(notesField.getText());
+    }
+
 }
+
+
