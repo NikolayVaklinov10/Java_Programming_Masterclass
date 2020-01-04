fb057f2d90e4e4f1f4b0b73e94cd8e5cf5850b9b
Nick <nikolayvaklinov@yahoo.com>
2020-01-04 22:22:07 +0200

    Move ,Rename and Delete

--- current-java/Paths/src/com/nickvaklinov/Main.java
+++ current-java/Paths/src/com/nickvaklinov/Main.java
@@ -11,12 +11,26 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            Path sourceFile = FileSystems.getDefault().getPath("Examples", "file1.txt");
-            Path copyFile = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
-            Files.copy(sourceFile, copyFile);
+            Path fileToDelete = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
+            Files.deleteIfExists(fileToDelete);
+
+//            Path fileToMove = FileSystems.getDefault().getPath("Examples", "file1.txt");
+//            Path destination = FileSystems.getDefault().getPath("Examples", "file1.txt");
+//            Files.move(fileToMove, destination);
+
+//            Path sourceFile = FileSystems.getDefault().getPath("Examples", "file1.txt");
+//            Path copyFile = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
+//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+//
+//            sourceFile = FileSystems.getDefault().getPath("Examples", "Dir1");
+//            copyFile = FileSystems.getDefault().getPath("Examples", "Dir4");
+//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+
         } catch(IOException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
+
+
     }
 //        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
 //        printFile(path);
