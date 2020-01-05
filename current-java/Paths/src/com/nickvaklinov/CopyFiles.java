77adca73f68c9e088538b6655dbcd592d2f71d5c
Nick <nikolayvaklinov@yahoo.com>
2020-01-05 15:11:34 +0200

    Mapping IO and NIO Methods

--- current-java/Paths/src/com/nickvaklinov/CopyFiles.java
+++ current-java/Paths/src/com/nickvaklinov/CopyFiles.java
@@ -8,6 +8,7 @@ import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 
 public class CopyFiles extends SimpleFileVisitor<Path> {
+
     private Path sourceRoot;
     private Path targetRoot;
 
