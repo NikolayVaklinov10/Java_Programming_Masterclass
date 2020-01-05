fcd461204a42f6c61f888f8affd06715d90f36d4
Nick <nikolayvaklinov@yahoo.com>
2020-01-05 13:45:04 +0200

    Separators Temp Files and File Stores

--- current-java/Paths/src/com/nickvaklinov/Main.java
+++ current-java/Paths/src/com/nickvaklinov/Main.java
@@ -1,6 +1,7 @@
 package com.nickvaklinov;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
@@ -18,7 +19,8 @@ public class Main {
 
         DirectoryStream.Filter<Path> filter = p -> Files.isRegularFile(p);
 
-        Path directory = FileSystems.getDefault().getPath("FileTree/Dir2");  // FileTree\\Dir2 (windows)
+        Path directory = FileSystems.getDefault().getPath("FileTree" + File.separator + "Dir2");
+//        Path directory = FileSystems.getDefault().getPath("FileTree/Dir2");  // FileTree\\Dir2 (windows)
         try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, filter)) {
             for (Path file : contents) {
                 System.out.println(file.getFileName());
@@ -27,5 +29,31 @@ public class Main {
         } catch (IOException | DirectoryIteratorException e) {
             System.out.println(e.getMessage());
         }
+
+        String separator = File.separator;
+        System.out.println(separator);
+        separator = FileSystems.getDefault().getSeparator();
+        System.out.println(separator);
+
+        try {
+            Path tempFile = Files.createTempFile("myapp", ".appext");  // C:\Users\???\AppData\Local\Temp\myapp1797805585146820741.appext
+            System.out.println("Temporary file path = " + tempFile.toAbsolutePath());
+
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        Iterable<FileStore> stores = FileSystems.getDefault().getFileStores();
+        for(FileStore store : stores) {
+            System.out.println("Volume name/Drive letter = " + store);
+            System.out.println("file store = " + store.name());
+        }
+
+        System.out.println("*******************");
+        Iterable<Path> rootPaths = FileSystems.getDefault().getRootDirectories();
+        for(Path path : rootPaths) {
+            System.out.println(path);
+        }
+
     }
 }
