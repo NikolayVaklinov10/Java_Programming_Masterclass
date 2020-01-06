2673d0481a5bf386df08b5398427ab28309d5fd2
Nick <nikolayvaklinov@yahoo.com>
2020-01-06 18:53:53 +0200

    Runnable and Thread

--- current-java/Threads/src/com/nickvaklinov/Main.java
+++ current-java/Threads/src/com/nickvaklinov/Main.java
@@ -1,7 +1,6 @@
 package com.nickvaklinov;
 
-import static com.nickvaklinov.ThreadColor.ANSI_GREEN;
-import static com.nickvaklinov.ThreadColor.ANSI_PURPLE;
+import static com.nickvaklinov.ThreadColor.*;
 
 public class Main {
 
@@ -9,13 +8,22 @@ public class Main {
         System.out.println(ANSI_PURPLE + "Hello from the main thread");
 
         Thread anotherThread = new AnotherThread();
+        anotherThread.setName("== Another Thread ==");
         anotherThread.start();
 
         new Thread(){
             public void run(){
-                System.out.println(ANSI_GREEN +"Hello from the anonymous class thread");
+                System.out.println(ANSI_GREEN +"Hello from anonymous class thread");
             }
         }.start();
+
+        Thread myRunnableThread = new Thread(new MyRunnable(){
+            @Override
+            public void run() {
+                System.out.println(ANSI_RED + " Hello from anonymous class's implementation of run()");
+            }
+        });
+        myRunnableThread.start();
 
         System.out.println(ANSI_PURPLE + "Hello again from the main thread");
 
