f02e0ab0936f34107dc32be95b7f9ae9f85963f9
Nick <nikolayvaklinov@yahoo.com>
2020-01-07 22:34:17 +0200

    Deadlocks

--- current-java/ProducerConsumer/src/com/nickvaklinov/Main.java
+++ current-java/ProducerConsumer/src/com/nickvaklinov/Main.java
@@ -8,107 +8,54 @@ import java.util.Random;
 import java.util.concurrent.*;
 import java.util.concurrent.locks.ReentrantLock;
 
-import static com.nickvaklinov.Main.EOF;
 
 
 public class Main {
-    public static final String EOF = "EOF";
+
+    public static Object lock1 = new Object();
+    public static Object lock2 = new Object();
 
     public static void main(String[] args) {
-        ArrayBlockingQueue<String> buffer = new ArrayBlockingQueue<String>(6);
-
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
-
-        MyProducer producer = new MyProducer(buffer, ThreadColor.ANSI_YELLOW);
-        MyConsumer consumer1 = new MyConsumer(buffer, ThreadColor.ANSI_PURPLE);
-        MyConsumer consumer2 = new MyConsumer(buffer, ThreadColor.ANSI_CYAN);
-
-       executorService.execute(producer);
-       executorService.execute(consumer1);
-       executorService.execute(consumer2);
-
-        Future<String> future = executorService.submit(new Callable<String>() {
-            @Override
-            public String call() throws Exception {
-                System.out.println(ThreadColor.ANSI_WHITE + "I'm being printed for the Callable class");
-                return "This is the callable result";
-            }
-        });
-
-        try {
-            System.out.println(future.get());
-        } catch(ExecutionException e) {
-            System.out.println("Something went wrong");
-        } catch(InterruptedException e) {
-            System.out.println("Thread running the task was interrupted");
-        }
-
-        executorService.shutdown();
+        new Thread1().start();
+        new Thread2().start();
 
     }
-}
 
-class MyProducer implements Runnable{
-    private ArrayBlockingQueue<String> buffer;
-    private String color;
-
-
-    public MyProducer(ArrayBlockingQueue<String> buffer, String color) {
-        this.buffer = buffer;
-        this.color = color;
-    }
-
-    public void run(){
-        Random random = new Random();
-        String[] nums = {"1", "2","3","4","5"};
-
-        for(String num: nums) {
-            try {
-                System.out.println(color + "Adding..." + num);
-                buffer.put(num);
-
-                Thread.sleep(random.nextInt(1000));
-            } catch(InterruptedException e) {
-                System.out.println("Producer was interrupted");
-            }
-        }
-        System.out.println(color + "Adding EOF and exiting....");
-        try {
-            buffer.put("EOF");
-        } catch (InterruptedException e){
-        }
-
-    }
-}
-
-class MyConsumer implements Runnable {
-    private ArrayBlockingQueue<String> buffer;
-    private String color;
-
-    public MyConsumer(ArrayBlockingQueue<String> buffer, String color) {
-        this.buffer = buffer;
-        this.color = color;
-    }
-
-    public void run() {
-
-        while(true) {
-            synchronized (buffer) {
+    private static class Thread1 extends Thread {
+        public void run() {
+            synchronized (lock1) {
+                System.out.println("Thread 1: Has lock1");
                 try {
-                    if (buffer.isEmpty()) {
-                        continue;
-                    }
-
-                    if (buffer.peek().equals(EOF)) {
-                        System.out.println(color + "Exiting");
-                        break;
-                    } else {
-                        System.out.println(color + "Removed " + buffer.take());
-                    }
-                } catch (InterruptedException e) {
+                    Thread.sleep(100);
+                } catch(InterruptedException e) {
 
                 }
+                System.out.println("Thread 1: Waiting for lock 2");
+                synchronized (lock2) {
+                    System.out.println("Thread 1: Has lock1 and lock2");
+                }
+                System.out.println("Thread 1: Released lock2");
             }
+            System.out.println("Thread 1: Released lock1. Exiting...");
+        }
+    }
+
+    private static class Thread2 extends Thread {
+        public void run() {
+            synchronized (lock1) {
+                System.out.println("Thread 2: Has lock1");
+                try {
+                    Thread.sleep(100);
+                } catch(InterruptedException e) {
+
+                }
+                System.out.println("Thread 2: Waiting for lock2");
+                synchronized (lock2) {
+                    System.out.println("Thread 2: Has lock1 and lock2");
+                }
+                System.out.println("Thread 2: released lock2");
+            }
+            System.out.println("Thread 2: Released lock1. Exiting...");
         }
     }
 }
