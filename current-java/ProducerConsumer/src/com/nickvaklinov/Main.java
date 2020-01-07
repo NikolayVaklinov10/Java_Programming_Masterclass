7db05ce2e49e7b002767bdc2b524592a32ee8b31
Nick <nikolayvaklinov@yahoo.com>
2020-01-07 23:07:06 +0200

    More Deadlocks

--- current-java/ProducerConsumer/src/com/nickvaklinov/Main.java
+++ current-java/ProducerConsumer/src/com/nickvaklinov/Main.java
@@ -12,56 +12,57 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
 
-    public static Object lock1 = new Object();
-    public static Object lock2 = new Object();
-
     public static void main(String[] args) {
-        new Thread1().start();
-        new Thread2().start();
+        final PolitePerson jane = new PolitePerson("Jane");
+        final PolitePerson john = new PolitePerson("John");
 
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                jane.sayHello(john);
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                john.sayHello(jane);
+            }
+        }).start();
     }
 
-    private static class Thread1 extends Thread {
-        public void run() {
-            synchronized (lock1) {
-                System.out.println("Thread 1: Has lock1");
-                try {
-                    Thread.sleep(100);
-                } catch(InterruptedException e) {
+    // 1. Thread1 acquires the lock on the jane object and enters the sayHello() method.
+    // It prints to the console, then suspends.
+    // 2. Thread2 acquires the lock on the john object and enters the sayHello() method.
+    // It prints to the console, then suspends.
+    // 3. Thread1 runs again and wants to say hello back to the john object. It tries to call the sayHelloBack() method
+    // using the john object that was passed into the sayHello() method,
+    // but Thread2 is holding the john lock, so Thread1 suspends.
+    // 4. Thread2 runs again and wants to say hello back to the jane object. It tries to call the sayHelloBack() method
+    // using the jane object that was passed into the sayHello() method,
+    // but Thread1 is holding the jane lock, so Thread2 suspends.
 
-                }
-                System.out.println("Thread 1: Waiting for lock 2");
-                synchronized (lock2) {
-                    System.out.println("Thread 1: Has lock1 and lock2");
-                }
-                System.out.println("Thread 1: Released lock2");
-            }
-            System.out.println("Thread 1: Released lock1. Exiting...");
+    static class PolitePerson {
+        private final String name;
+
+        public PolitePerson(String name) {
+            this.name = name;
         }
-    }
 
-    private static class Thread2 extends Thread {
-        public void run() {
-            synchronized (lock1) {
-                System.out.println("Thread 2: Has lock1");
-                try {
-                    Thread.sleep(100);
-                } catch(InterruptedException e) {
+        public String getName() {
+            return name;
+        }
 
-                }
-                System.out.println("Thread 2: Waiting for lock2");
-                synchronized (lock2) {
-                    System.out.println("Thread 2: Has lock1 and lock2");
-                }
-                System.out.println("Thread 2: released lock2");
-            }
-            System.out.println("Thread 2: Released lock1. Exiting...");
+        public synchronized void sayHello(PolitePerson person) {
+            System.out.format("%s: %s" + " has said hello to me!%n", this.name, person.getName());
+            person.sayHelloBack(this);
+        }
+
+        public synchronized void sayHelloBack(PolitePerson person) {
+            System.out.format("%s: %s" + " has said hello back to me!%n", this.name, person.getName());
         }
     }
 }
-
-
-
 
 
 
