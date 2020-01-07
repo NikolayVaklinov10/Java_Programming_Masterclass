eec9299bfb17617251f25adeb8ecbdece8f973b0
Nick <nikolayvaklinov@yahoo.com>
2020-01-07 17:21:48 +0200

    Java Util concurrent package

---
+++ current-java/ProducerConsumer/src/com/nickvaklinov/Main.java
@@ -0,0 +1,73 @@
+package com.nickvaklinov;
+
+import jdk.internal.dynalink.linker.LinkerServices;
+
+import java.util.List;
+import java.util.Random;
+
+import static jdk.nashorn.internal.parser.TokenType.EOF;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+
+class MyProducer implements Runnable{
+    private List<String> buffer;
+    private String color;
+
+    public MyProducer(List<String> buffer, String color) {
+        this.buffer = buffer;
+        this.color = color;
+    }
+
+    public void run(){
+        Random random = new Random();
+        String[] nums = {"1", "2","3","4","5"};
+
+        for(String num: nums) {
+            try {
+                System.out.println(color + "Adding..." + num);
+                buffer.add(num);
+
+                Thread.sleep(random.nextInt(1000));
+            } catch(InterruptedException e) {
+                System.out.println("Producer was interrupted");
+            }
+        }
+        System.out.println(color + "Adding EOF and exiting....");
+        buffer.add("EOF");
+    }
+}
+
+class MyConsumer implements Runnable {
+    private List<String> buffer;
+    private String color;
+
+    public MyConsumer(List<String> buffer, String color) {
+        this.buffer = buffer;
+        this.color = color;
+    }
+
+    public void run() {
+        while(true) {
+            if(buffer.isEmpty()) {
+                continue;
+            }
+            if(buffer.get(0).equals(EOF)) {
+                System.out.println(color + "Exiting");
+                break;
+            } else {
+                System.out.println(color + "Removed " + buffer.remove(0));
+            }
+        }
+    }
+}
+
+
+
+
+
+
