8b5dfdfaae1e5a6b28c562123926946b4e2d59d8
Nick <nikolayvaklinov@yahoo.com>
2020-01-19 14:19:25 +0000

    Functions and Views

--- current-java/Music/src/com/nickvaklinov/Main.java
+++ current-java/Music/src/com/nickvaklinov/Main.java
@@ -2,13 +2,13 @@ package com.nickvaklinov;
 
 import com.nickvaklinov.model.Artist;
 import com.nickvaklinov.model.Datasource;
+import com.nickvaklinov.model.SongArtist;
 
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-
         Datasource datasource = new Datasource();
         if(!datasource.open()) {
             System.out.println("Can't open datasource");
@@ -25,6 +25,33 @@ public class Main {
             System.out.println("ID = " + artist.getId() + ", Name = " + artist.getName());
         }
 
+        List<String> albumsForArtist =
+                datasource.queryAlbumsForArtist("Carole King", Datasource.ORDER_BY_ASC);
+
+        for(String album : albumsForArtist) {
+            System.out.println(album);
+        }
+
+        List<SongArtist> songArtists = datasource.queryArtistsForSong("Go Your Own Way", Datasource.ORDER_BY_ASC);
+        if(songArtists == null) {
+            System.out.println("Couldn't find the artist for the song");
+            return;
+        }
+
+        for(SongArtist artist : songArtists) {
+            System.out.println("Artist name = " + artist.getArtistName() +
+                    " Album name = " + artist.getAlbumName() +
+                    " Track = " + artist.getTrack());
+        }
+
+        datasource.querySongsMetadata();
+
+        int count = datasource.getCount(Datasource.TABLE_SONGS);
+        System.out.println("Number of songs is: " + count);
+
+        datasource.createViewForSongArtists();
+
         datasource.close();
     }
 }
+
